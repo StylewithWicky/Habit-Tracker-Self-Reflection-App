d4fd42a8b4d82b796c6a063e9e759f0aa9ea55e9
Yazz <itsjustmaitai@gmail.com>
2025-06-30 18:38:29 +0300

    Registered blueprints,initialised the db

---
+++ Backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_cors import CORS
+from models import db
+from config import Config
+from models.auth_routes import user_bp
+
+app=Flask(__name__)
+app.config.from_object(Config)
+
+CORS(app)
+db.init_app(app)
+
+@app.register_blueprint(user_bp)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+if  __name__=='__main__':
+    app.run(debug=True)
